eefc8aa8212d1c5520fdad5894d4caf60ca39b25
Brian Carter <robotmachine@gmail.com>
2016-11-18 15:40:24 -0500

    Added Outlook CLI tools

---
+++ ov_outlook_cli.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Common Files\microsoft shared\VSTO\10.0\VSTOInstaller.exe" /install https://builds.citrixonlinecdn.com/builds/calendarintegration/outlook/openvoice/OpenVoiceForOutlook.vsto
